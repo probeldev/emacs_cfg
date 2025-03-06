5c8459d9a06aecf48cead42c256dc3f5c136d1c3
Sergey Protko <sergey.protko@yandex.ru>
2025-03-06 08:09:40 +0300

    fix fonts

--- font.el
+++ font.el
@@ -1,7 +1,5 @@
-;; Установка шрифта Fira Code
-;; (set-frame-font "Fira Code-11" nil t) ; 12 — размер шрифта, измените на нужный
-;; Установка FiraCode Nerd Font
-(set-frame-font "FiraCode Nerd Font Mono-11" nil t)  ;; Размер шрифта 12
+;; Настройка шрифта для новых фреймов
+(add-to-list 'default-frame-alist '(font . "FiraCode Nerd Font Mono-11"))
 
 ;; Установка ligature (если ещё не установлен)
 (unless (package-installed-p 'ligature)
@@ -30,27 +28,7 @@
 (add-hook 'dired-mode-hook (lambda () (display-line-numbers-mode -1)))
 (add-hook 'term-mode-hook (lambda () (display-line-numbers-mode -1)))
 
-
 ;; Установка all-the-icons для иконок
 (use-package all-the-icons
   :ensure t
   :if (display-graphic-p))
-;; Установка ligature (если ещё не установлен)
-(unless (package-installed-p 'ligature)
-  (package-refresh-contents)
-  (package-install 'ligature))
-
-;; Включение поддержки лигатур
-(require 'ligature)
-(ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "||=" "||>"
-                                     ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/="
-                                     "!==" "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->"
-                                     "---" "-<<" "<--" "<---" "<->>" "<+>" "</>" "###" "#_("
-                                     "..<" "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~"
-                                     "~@" "~=" "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|="
-                                     "|>" "|-" "{|" "[|" "]#" "::" ":=" ":>" ":<" "$>" "=="
-                                     "=>" "!=" "!!" ">=" ">>" "<=" "<>" "<|" "<$" "<*" "<+"
-                                     "</" "#{" "#[" "#:" "#=" "#!" "##" "#(" "#?" "#_" "%%"
-                                     ".=" ".-" ".." ".?" "+>" "++" "?:" "?=" "?." "??" ";;"
-                                     "/*" "/=" "/>" "//" "__" "~~" "(*" "*)" "\\\\" "://"))
-(global-ligature-mode 1)
